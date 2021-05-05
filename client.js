e2d26aa960bfbcaf907bdb8401e41fd11ebaff35
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-05 17:43:16 -0400

    cile file created, function exported

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// Establishes Conenction with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  conn.on('data', (data) => {
+    console.log(data)
+  });
+  
+  conn.setEncoding("utf8");
+  // console.log(conn);
+  return conn;
+  
+};
+
+console.log("Connecting...");
+connect();
+
+module.exports = {connect}
